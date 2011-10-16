00d69805b8565f7989709b52c57dbfb2566084be
Jon Haugsand <jon.haugsand@gmail.com>
2011-10-16 18:12:23 +0200

    More close board

--- test/board-test.js
+++ test/board-test.js
@@ -13,9 +13,19 @@ var testBoard =
     '.........\n' +
     '.........\n';
 
-exports['Simple'] = function (test) {
-    var b = new Board(testBoard);
+exports['One line'] = function (test) {
+    var b = new Board();
+    var arr = b.row2arr('..2.59...');
+    console.log('arr=' + arr);
+    test.equal(2,arr[2]);
+    test.done();
+};
 
-    test.equal(2,b.getValue(3,0));
+exports['Full board'] = function(test) {
+    var b = new Board(testBoard);
+    console.log(b);
+    var pos = new Pos(1,4);
+    console.log('4,1=' + b.get(pos));
+    test.equal(1,b.get(pos));
     test.done();
 };
